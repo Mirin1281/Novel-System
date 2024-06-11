2deeb30b539464d95dd82e95c372d16391bbe290
Mirin <113825778+Mirin1281@users.noreply.github.com>
2024-06-12 00:37:38 +0900

    ゲーム内リファレンスとりあえず完成

--- Assets/Novel/Scripts/Utility/ConstContainer.cs
+++ Assets/Novel/Scripts/Utility/ConstContainer.cs
@@ -7,7 +7,7 @@ namespace Novel
     public static class ConstContainer
     {
         public static readonly string NOVEL_PATH = DirectoryPathGetter.GetNovelFolderPath();
-        public static readonly string COMMANDDATA_PATH = NOVEL_PATH + "/Scriptable/Commands";
+        public static readonly string COMMANDDATA_PATH = NOVEL_PATH + "/Scriptables/Commands";
 
         public const string SUBMIT_KEYNAME = "Submit";
         public const string CANCEL_KEYNAME = "Cancel";
